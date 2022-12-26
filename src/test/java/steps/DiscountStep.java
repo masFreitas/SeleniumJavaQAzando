28913f17158eb45287b6a76adfd8083a2bbe0b15
Mateus GitHub <mateusasfreitas@gmail.com>
2022-12-26 15:12:05 -0300

    Initializing driver and writing first step

--- src/test/java/steps/DiscountStep.java
+++ src/test/java/steps/DiscountStep.java
@@ -1,32 +1,42 @@
 package steps;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DiscountStep {
 
+    WebDriver driver = new ChromeDriver();
+
     @Given("^that Im on qazando website$")
     public void that_Im_on_qazando_website() throws Throwable {
-        System.out.println("chegou");
+        driver.get("https://www.qazando.com.br/curso.html");
+        driver.manage().window().maximize();
     }
 
     @When("^I fill email field$")
     public void i_fill_email_field() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+        jse.executeScript("window.scrollTo(0, 20000)");
+        Thread.sleep(2000);
+
+        driver.findElement(By.id("email")).sendKeys("teste@teste.com");
     }
 
     @When("^click on ganhar cupom button$")
     public void click_on_ganhar_cupom_button() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        driver.findElement(By.id("button")).click();
     }
 
     @Then("^I see the discount code$")
     public void i_see_the_discount_code() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        String textCoupon = driver.findElement(By.cssSelector("#cupom > h2 > span")).getText();
+        System.out.println(textCoupon);
+
+        driver.quit();
     }
 }
---
+++ target/test-classes/features/discount.feature
@@ -0,0 +1,12 @@
+# language: en
+
+Feature: Receive discount coupon
+  Me as a user from qazando
+  I want to receive a discount coupon
+  To buy a course with reduced price
+
+  Scenario: See coupon code
+    Given that Im on qazando website
+    When I fill email field
+    And click on ganhar cupom button
+    Then I see the discount code
